932f923ad3f1414660792cd50dbb03a94c351989
antonionduarte <an.duarte@campus.fct.unl.pt>
2022-07-25 01:31:26 +0000

    Login and register

--- src/auth.py
+++ src/auth.py
@@ -21,5 +21,43 @@ def register():
         elif not password: 
             error = 'Password is required'
 
+        if error is None:
+            try: 
+                database.execute(
+                    "INSERT INTO user (username, password) VALUES (?, ?)",
+                    (username, generate_password_hash(password)),
+                )
+                database.commit()
+            except database.IntegrityError:
+                error = f"User {username} is already registered."
+            else: 
+                return redirect(url_for("auth.login"))
+
+            flash(error)
     return render_template('auth/register.html')
+
+
+@blueprint.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        database = get_db()
+        error = None
+        user = database.execute(
+                'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None: 
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+
+        flash(error)
+    return render_template('auth/login.html')
 
