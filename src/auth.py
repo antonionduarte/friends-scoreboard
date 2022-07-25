9c68016c3a418f74529033db26b43a8d21e81cc5
antonionduarte <an.duarte@campus.fct.unl.pt>
2022-07-25 00:03:35 +0000

    Auth basics

---
+++ src/auth.py
@@ -0,0 +1,25 @@
+import functools 
+from flask import (
+        Blueprint, flash, g, redirect, render_template, 
+        request, session, url_for 
+)
+from werkzeug.security import check_password_hash, generate_password_hash 
+from src.database import get_db
+
+blueprint = Blueprint('auth', __name__, url_prefix='/auth')
+
+@blueprint.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        database = get_db()
+        error = None
+
+        if not username: 
+            error = 'Username is required.'
+        elif not password: 
+            error = 'Password is required'
+
+    return render_template('auth/register.html')
+
